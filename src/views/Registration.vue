faf4b859a1cab3d1c4cee7afac62389aef3975b2
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:59:37 +0800

    Added Registartion view

---
+++ src/views/Registration.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <registration></registration>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Registration from "@/components/Forms/Registration";
+
+export default {
+  components: {Registration}
+}
+</script>
