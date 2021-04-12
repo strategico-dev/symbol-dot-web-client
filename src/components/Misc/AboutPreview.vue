113e2737f40e0961108e26c8c42174467f50912b
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:57:35 +0800

    Added AboutPreview component

---
+++ src/components/Misc/AboutPreview.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="background preview-container">
+      <div>
+        <h3 class="header text-center">Symbol Dot CRM</h3>
+      </div>
+      <div>
+        <img src="assets/img/crm-preview.png" class="img-fluid">
+      </div>
+      <div>
+        <span class="text">Бесплатная CRM с открытым исходным кодом для вашего бизнеса и личных финансов.</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "AboutPreview"
+}
+</script>
