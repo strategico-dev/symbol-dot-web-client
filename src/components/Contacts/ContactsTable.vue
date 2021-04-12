331fab98defe0183c85f0e7e77a9ccd12fde2be2
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:58:50 +0800

    Added ContactsTable component

---
+++ src/components/Contacts/ContactsTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <table-themes></table-themes>
+    </div>
+    <table class="table"
+      :class="currentTheme"
+    >
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">ФИО</th>
+        <th scope="col">Телефон</th>
+        <th scope="col">Почта</th>
+      </tr>
+      </thead>
+      <tbody>
+        <contact-table-item></contact-table-item>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+import TableThemes from "@/components/Contacts/TableThemes";
+import ContactTableItem from "@/components/Contacts/ContactTableItem";
+
+export default {
+name: "ContactsTable",
+  components: {ContactTableItem, TableThemes},
+  computed: mapGetters(['currentTheme'])
+}
+</script>
+
+<style scoped>
+.table {
+  margin-top: 10px;
+}
+</style>
