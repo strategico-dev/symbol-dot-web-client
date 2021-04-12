5c80ef71a99bd21b7490e984b23f89d18db73c04
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:59:05 +0800

    Added TableThemes component

---
+++ src/components/Contacts/TableThemes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      |
+      <span
+          v-for="theme in themes"
+          @click="changeTheme(theme)"
+          class="theme text"
+      >
+        <span :class="{'theme-active': theme.active}">{{theme.name}}</span> |
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "TableThemes",
+  computed: mapGetters(['themes']),
+  methods: {
+    changeTheme(theme) {
+      this.$store.commit('setCurrentTheme', theme.className);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.theme {
+  color: blue;
+  cursor: pointer;
+}
+.theme-active {
+  background: #ff7700;
+  color: #fff;
+}
+</style>
