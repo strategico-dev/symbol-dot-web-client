ae4b20204753b23272161f0cc2b90753d6890c0b
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:59:29 +0800

    Added Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <login></login>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from "@/components/Forms/Login";
+
+export default {
+  components: {Login}
+}
+</script>
