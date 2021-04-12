8d346d172a9f4fb111b87a546239f02af3d48500
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:56:27 +0800

    Added ContactSearch component

---
+++ src/components/Forms/ContactSearch.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-sm-5">
+        <input
+            class="form-control text"
+            placeholder="Поиск по ФИО"
+        >
+      </div>
+      <div class="col-lg-2 col-sm-7">
+        <button class="btn btn-primary search-button header">Поиск</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactSearch"
+}
+</script>
+
+<style scoped>
+@media (max-width: 768px) {
+  .search-button {
+    margin-top: 10px;
+  }
+}
+</style>
