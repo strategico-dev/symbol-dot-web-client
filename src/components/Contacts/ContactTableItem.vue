437d6d22294f3ab3e28dc7de697bd78e7bda686b
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:58:35 +0800

    Added ContactTableItem component

---
+++ src/components/Contacts/ContactTableItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <th scope="row">3</th>
+    <td>Larry</td>
+    <td>the Bird</td>
+    <td>@twitter</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ContactTableItem"
+}
+</script>
+
+<style scoped>
+
+</style>
