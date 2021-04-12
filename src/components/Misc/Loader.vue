f9cba4baa27d3d18ae326ec2b3620a5536c01614
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-12 19:57:47 +0800

    Added Loader

---
+++ src/components/Misc/Loader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="lds-dual-ring"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader"
+}
+</script>
+
+<style scoped>
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #0099ff;
+  border-color: #0099ff transparent #0099ff transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
