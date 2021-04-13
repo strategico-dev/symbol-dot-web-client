7a271860b226e8bba4a48304a53d119a64560be6
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-13 15:24:02 +0800

    Added Employees component

---
+++ src/components/Companies/Employees.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="employees">
+    <employee></employee>
+    <employee></employee>
+    <employee></employee>
+    <employee></employee>
+    <employee></employee>
+  </div>
+</template>
+
+<script>
+import Employee from "@/components/Companies/Employee";
+export default {
+  name: "Employees",
+  components: {Employee}
+}
+</script>
+
+<style scoped>
+.employees {
+  max-height: 200px;
+  overflow-y: scroll;
+  background: #e6f5ff;
+  border: 1px solid #000;
+}
+.row {
+  margin-bottom: 10px;
+}
+</style>
