ffd358c10fa8c5db48b1324cba59d6ffc56dcb4a
Artem Tyutnev <strategico.dev@gmail.com>
2021-04-13 15:23:32 +0800

    Added CompanyPreview component

---
+++ src/components/Companies/CompanyPreview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="company-preview">
+      <div>
+        <h3 class="header">
+          <router-link to="/companies/1">Наименование</router-link>
+        </h3>
+      </div>
+      <div>
+        <span class="text">ОГРН: 12345678</span>
+      </div>
+      <div>
+        <span class="text">ИНН: 12345678</span>
+      </div>
+      <div>
+        <span class="text">КПП: 12345678</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompanyPreview"
+}
+</script>
+
+<style scoped>
+.company-preview {
+  background: #fff;
+  padding: 10px;
+  -webkit-box-shadow: 1px 1px 5px 0 rgba(0,0,0,0.75);
+  -moz-box-shadow: 1px 1px 5px 0 rgba(0,0,0,0.75);
+  box-shadow: 1px 1px 5px 0 rgba(0,0,0,0.75);
+}
+</style>
